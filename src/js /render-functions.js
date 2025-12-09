8c112d254d1cb3732c6cc2cb6abad8201549f503
maksimVoloshchuk <maksimvolosuk684@gmail.com>
2025-12-09 21:20:15 +0100

    Update render-functions.js

--- src/js /render-functions.js
+++ src/js /render-functions.js
@@ -1,6 +1,4 @@
-// Описаний у документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const lightbox = new SimpleLightbox('.gallery a', { /* options */ );
